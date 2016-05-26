c703dc4a6ed4af3005d49bf3bd114deb324df178
tinray <b03902072@ntu.edu.tw>
2016-05-26 20:30:49 +0800

    click to add gate

--- js/gui/schematic.js
+++ js/gui/schematic.js
@@ -2,8 +2,8 @@ var GUI = GUI || {};
 GUI.schematic = GUI.schematic || {};
 
 GUI.schematic.init = function(gateListView, paperView){
-    this._initGateListView(gateListView);
     this._initPaper(paperView);
+    this._initGateListView(gateListView);
 };
 
 GUI.schematic._initGateListView = function(gateListView){
@@ -12,6 +12,78 @@ GUI.schematic._initGateListView = function(gateListView){
         let li = $('<li></li>');
         li.append($('<img/>').attr('src', gate.img));
         li.append($('<span>' + gate.name + '</span>'));
+
+        // dirty closure...
+        (function(gateName){
+            li.click(function(){
+                GUI.schematic.insertGate(30, 30, gateName);
+            });
+        })(gateName);
+        
         gateListView.append(li);
     }
+};
+
+GUI.schematic._initPaper = function(paperView){
+    this.graph = new joint.dia.Graph();
+    
+    this.paper = new joint.dia.Paper({
+        el: paperView,
+        width: paperView.width(),
+        height: paperView.height(),
+        gridSize: 1,
+        model: this.graph,
+        linkView: joint.dia.LinkView.extend({
+            pointerclick: function(evt, x, y) {
+                
+            }
+        }),
+        interactive: function(cellView) {
+            if (cellView.model instanceof joint.dia.Link) {
+                // Disable the default vertex add functionality on pointerdown.
+                return { vertexAdd: false };
+            }
+            return true;
+        }
+    });
+    
+    // disable contexmenu
+    this.paper.$el.on('contextmenu', function(evt) { 
+        evt.preventDefault();  
+        var cellView = paper.findView(evt.target);
+        if (cellView) {
+            console.log(cellView.model.id);  // So now you have access to both the cell view and its model.
+            // ... display custom context menu, ...
+        }
+    });
+
+    this.paper.on('cell:pointerdblclick', function(cellView, evt) {
+        console.log("cellview:", cellView);
+        let cell = graph.getCell(cellView.model.attributes.id);
+        if( cell.isLink() ){
+            cell.label(0, {attrs: { text: { text: 'rested!!' } }} );
+        }
+        
+    });
+
+    this.paper.on('cell:pointerclick', function(cellView, evt) {
+        if( cellView.model.attributes.type == 'fsa.State'){
+            cellView.highlight();
+        }
+        console.log("click!!", evt, cellView);
+    });
+
+};
+
+GUI.schematic.insertGate = function(x, y, gateName){
+    var cell = new joint.shapes.gate.Gate({
+        position: { x: x, y: y },
+        inPorts: ['in'],
+        outPorts: ['out'],
+        attrs: {
+            image: { 'xlink:href': Gate[gateName].img }
+        }
+    });
+    this.graph.addCell(cell);
+
 };
