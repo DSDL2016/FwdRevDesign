4587edd8a522db83ef8d129bec8202ac6603d57b
tinray <b03902072@ntu.edu.tw>
2016-05-29 21:14:58 +0800

    add click to select state

--- js/gui/fsm.js
+++ js/gui/fsm.js
@@ -5,7 +5,7 @@ GUI.fsm.init = function(paperView){
 };
 
 GUI.fsm._initPaper = function(paperView){
-    this.selected = undefined;
+    this.selected = "";
     
     this.graph = new joint.dia.Graph();
     
@@ -28,7 +28,7 @@ GUI.fsm._initPaper = function(paperView){
     // disable contexmenu
     this.paper.$el.on('contextmenu', function(evt) { 
         evt.preventDefault();  
-        var cellView = GUI.schematic.paper.findView(evt.target);
+        var cellView = GUI.fsm.paper.findView(evt.target);
         if (cellView) {
             console.log(cellView.model.id);  // So now you have access to both the cell view and its model.
             // ... display custom context menu, ...
@@ -38,19 +38,21 @@ GUI.fsm._initPaper = function(paperView){
 
     // register click to highlight (select)
     this.paper.on('cell:pointerclick', function(cellView, evt) {
-        if( cellView.model.attributes.type == 'gate.Gate'){
-            let gate = GUI.schematic.graph.getCell(cellView.model.id);
-            if( cellView.model.id !== GUI.schematic.selected ){
-                if( GUI.schematic.selected ){
-                    GUI.schematic.graph.getCell(GUI.schematic.selected).removeAttr('image/filter');
-                    GUI.schematic.selected = undefined;                
+        if( cellView.model.attributes.type == 'fsm.State'){
+            let state = GUI.fsm.graph.getCell(cellView.model.id);
+            if( GUI.fsm.selected ){
+                if( GUI.fsm.selected !== cellView.model.id ){
+                    GUI.fsm.newLink(GUI.fsm.selected, cellView.model.id);
+                    GUI.fsm.selected = undefined;
                 }
-                gate.attr('image/filter', { name: 'dropShadow', args: {dx: 4, dy: 4, blur: 5, color: '#26b14f'} });
-                GUI.schematic.selected = cellView.model.id;
+                else {
+                    state.removeAttr('circle/filter');
+                    GUI.fsm.selected = undefined;                
+                }
             }
-            else {
-                gate.removeAttr('image/filter');
-                GUI.schematic.selected = undefined;                
+            else{
+                state.attr('circle/filter', { name: 'dropShadow', args: {dx: 4, dy: 4, blur: 5, color: '#26b14f'} });
+                GUI.fsm.selected = cellView.model.id;
             }
         }
         console.log("click!!", evt, cellView);
@@ -58,9 +60,9 @@ GUI.fsm._initPaper = function(paperView){
 
     $(Document).keypress(function(evt){
         if( evt.key === 'Delete'){
-            if( GUI.schematic.selected ){
-                GUI.schematic.graph.getCell(GUI.schematic.selected).remove();
-                GUI.schematic.selected = undefined;
+            if( GUI.fsm.selected ){
+                GUI.fsm.graph.getCell(GUI.fsm.selected).remove();
+                GUI.fsm.selected = undefined;
             }
         }
     });
@@ -72,5 +74,10 @@ GUI.fsm.newState = function(x, y){
         position: { x: x, y: y }
     });
     this.graph.addCell(cell);
-
 };
+
+
+GUI.fsm.newLink = function(id1, id2){
+};
+
+
