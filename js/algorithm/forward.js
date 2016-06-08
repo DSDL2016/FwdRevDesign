1268f1a94fc5b656771765ea569a6e05d910cf3f
Bookgin <dongsheoil@gmail.com>
2016-06-08 09:17:07 +0800

    Refine codes with more readable naming style

--- js/algorithm/forward.js
+++ js/algorithm/forward.js
@@ -1,12 +1,8 @@
 var Algorithm = Algorithm || {};
 Algorithm.Fsm2schematic = Algorithm.Fsm2schematic || {};
-
-Algorithm.forward = function(fsm) {
-  return Algorithm.Fsm2schematic.convert(fsm);
-};
-
+var Fsm2schematic = Algorithm.Fsm2schematic;
 /**
- * Algorithm.Fsm2schematic.convert() returns schematic based on the passed final state machine
+ * Algorithm.forward() returns schematic based on the passed final state machine
  *
  * @param {Object} fsm = {
  *     "10": [{next: "10", out: "0"}, {next: "10", out: "0"}],
@@ -22,92 +18,96 @@ Algorithm.forward = function(fsm) {
  *   ...
  * }
  */
-Algorithm.Fsm2schematic.convert = function(fsm) {
-  var bitLength = Algorithm.Fsm2schematic.getBitLength(fsm);
-  var stateTruthTables = Algorithm.Fsm2schematic.fsm2stateTruthTable(fsm, bitLength);
-  var outputTruthTable = Algorithm.Fsm2schematic.fsm2outputTruthTable(fsm);
-
-  Algorithm.Fsm2schematic.init(bitLength)
-  Algorithm.Fsm2schematic.addStateTruthTables(stateTruthTables);
-  Algorithm.Fsm2schematic.addOutputTruthTable(outputTruthTable);
-
-  return Algorithm.Fsm2schematic.getGates();
+Algorithm.forward = function(fsm) {
+  return Fsm2schematic.convert(fsm);
 };
 
-Algorithm.Fsm2schematic.getBitLength = function(fsm) {
+Fsm2schematic.convert = function(fsm) {
+  var bitLength = Fsm2schematic.getBitLength(fsm);
+  var stateTruthTables = Fsm2schematic.fsm2stateTruthTable(fsm, bitLength);
+  var outputTruthTable = Fsm2schematic.fsm2outputTruthTable(fsm);
+
+  Fsm2schematic.init(bitLength)
+  Fsm2schematic.addStateTruthTables(stateTruthTables);
+  Fsm2schematic.addOutputTruthTable(outputTruthTable);
+
+  return Fsm2schematic.getGates();
+};
+
+Fsm2schematic.getBitLength = function(fsm) {
   var totalStates = Object.keys(fsm).length;
   var bitLength = (totalStates - 1).toString(2).length;
   return bitLength;
 };
 
-Algorithm.Fsm2schematic.addGate = function(gate, output) {
+Fsm2schematic.addGate = function(gate, output) {
   if (typeof output == "undefined")
-    Algorithm.Fsm2schematic[gate] = {out: [[]]};
+    Fsm2schematic[gate] = {out: [[]]};
   else
-    Algorithm.Fsm2schematic[gate] = {out: [[{name: output}]]};
+    Fsm2schematic[gate] = {out: [[{name: output}]]};
 };
 
-Algorithm.Fsm2schematic.addOutputAt = function(gate, output) {
-  Algorithm.Fsm2schematic[gate].out[0].push({name: output});
+Fsm2schematic.addOutputAt = function(gate, output) {
+  Fsm2schematic[gate].out[0].push({name: output});
 };
 
-Algorithm.Fsm2schematic.init = function(bitLength) {
-  Algorithm.Fsm2schematic.addGate("or output", "output");
-  Algorithm.Fsm2schematic.addGate("output");
-  Algorithm.Fsm2schematic.addGate("input", "not input");
-  Algorithm.Fsm2schematic.addGate("not input");
+Fsm2schematic.init = function(bitLength) {
+  Fsm2schematic.addGate("or output", "output");
+  Fsm2schematic.addGate("output");
+  Fsm2schematic.addGate("input", "not input");
+  Fsm2schematic.addGate("not input");
   for (let i = 0; i < bitLength; i++) {
-    Algorithm.Fsm2schematic.addGate("or " + i, "dff " + i);
-    Algorithm.Fsm2schematic.addGate("dff " + i, "not " + i);
-    Algorithm.Fsm2schematic.addGate("not " + i);
+    Fsm2schematic.addGate("or " + i, "dff " + i);
+    Fsm2schematic.addGate("dff " + i, "not " + i);
+    Fsm2schematic.addGate("not " + i);
   }
 }; 
 
 // add "101" into schematic
-Algorithm.Fsm2schematic.addTerm = function(term, outputIndex, termIndex) {
+Fsm2schematic.addTerm = function(term, outputIndex, termIndex) {
   if (outputIndex == "output") {
     var andGateName = "and output";
-    Algorithm.Fsm2schematic.addGate(andGateName,"or output");
+    Fsm2schematic.addGate(andGateName,"or output");
   } else {
     var andGateName = "and " + outputIndex + termIndex;
-    Algorithm.Fsm2schematic.addGate(andGateName,"or " + outputIndex);
+    Fsm2schematic.addGate(andGateName,"or " + outputIndex);
   }
 
   for (let i = 0; i < term.length - 1; i++) {
     if (term[i] == "x")
       continue;
-    Algorithm.Fsm2schematic.addOutputAt((term[i] == "1" ? "dff " + i: "not " + i), andGateName);
+    Fsm2schematic.addOutputAt((term[i] == "1" ? "dff " + i: "not " + i), andGateName);
   }
 
   // handle input
   if (term[term.length - 1] != "x")
-    Algorithm.Fsm2schematic.addOutputAt((term[term.length - 1] == "1" ? "input": "not input"), andGateName);
+    Fsm2schematic.addOutputAt((term[term.length - 1] == "1" ? "input": "not input"), andGateName);
 };
 
 // add ["101", "011", "1xx"] into schematic
-Algorithm.Fsm2schematic.addTerms = function(truthTable, outputIndex) {
+Fsm2schematic.addTerms = function(truthTable, outputIndex) {
   for (let i = 0; i < truthTable.length; i++)
-    Algorithm.Fsm2schematic.addTerm(truthTable[i], outputIndex, i);
+    Fsm2schematic.addTerm(truthTable[i], outputIndex, i);
 };
 
 // add states [["10x","011"],["001"]] into schematic
-Algorithm.Fsm2schematic.addStateTruthTables = function(truthTables) {
+Fsm2schematic.addStateTruthTables = function(truthTables) {
   for (let i = 0; i < truthTables.length; i++)
-    Algorithm.Fsm2schematic.addTerms(truthTables[i], i);
+    Fsm2schematic.addTerms(truthTables[i], i);
 };
 
 // add output ["01x"] into schematic
-Algorithm.Fsm2schematic.addOutputTruthTable = function(truthTable) {
-  Algorithm.Fsm2schematic.addTerms(truthTable, "output");
+Fsm2schematic.addOutputTruthTable = function(truthTable) {
+  Fsm2schematic.addTerms(truthTable, "output");
 };
 
 // return gates object only rather than functions
-Algorithm.Fsm2schematic.getGateObjects = function() {
+Fsm2schematic.getGateObjects = function() {
   var gates = {};
-  for (let obj in Algorithm.Fsm2schematic) {
-    if (typeof Algorithm.Fsm2schematic[obj] == "function")
+  for (let obj in Fsm2schematic) {
+    if (typeof Fsm2schematic[obj] == "function")
       continue;
-    gates[obj] = Algorithm.Fsm2schematic[obj];
+    gates[obj] = Fsm2schematic[obj];
   }
   return gates;
 };
@@ -119,29 +119,29 @@ Algorithm.Fsm2schematic.getGateObjects = function() {
  *    ...
  *  }
  */
-Algorithm.Fsm2schematic.getGates = function() {
-  var gates = Algorithm.Fsm2schematic.getGateObjects();
-  gates = Algorithm.Fsm2schematic.removeNoOutputGates(gates);
-  gates = Algorithm.Fsm2schematic.removeUselessOutput(gates);
+Fsm2schematic.getGates = function() {
+  var gates = Fsm2schematic.getGateObjects();
+  gates = Fsm2schematic.removeNoOutputGates(gates);
+  gates = Fsm2schematic.removeUselessOutput(gates);
   var keys = Object.keys(gates);
   var pinNumCounter = new Array(keys.length).fill(0);
   var arr = [];
   for (let i in gates) {
     gates[i]["type"] = i.split(" ")[0];
-    arr.push(Algorithm.Fsm2schematic.parseGate(gates[i], keys, pinNumCounter));
+    arr.push(Fsm2schematic.parseGate(gates[i], keys, pinNumCounter));
   }
   return arr;
 };
 
 // parse a whole gate outputs
-Algorithm.Fsm2schematic.parseGate = function(gate, keys, pinNumCounter) {
+Fsm2schematic.parseGate = function(gate, keys, pinNumCounter) {
   for (let j = 0; j < gate.out.length; j++)
-    gate.out[j] = Algorithm.Fsm2schematic.parsePins(gate.out[j], keys, pinNumCounter);
+    gate.out[j] = Fsm2schematic.parsePins(gate.out[j], keys, pinNumCounter);
   return gate;
 };
 
 // parse each output pin
-Algorithm.Fsm2schematic.parsePins = function(pins, keys, pinNumCounter) {
+Fsm2schematic.parsePins = function(pins, keys, pinNumCounter) {
   for (let i = 0; i < pins.length; i++) {
     var index = keys.indexOf(pins[i].name);
     delete pins[i].name;
@@ -156,7 +156,7 @@ Algorithm.Fsm2schematic.parsePins = function(pins, keys, pinNumCounter) {
   * fsm {"10": [{next: "00", out: "0"}, {next: "10", out: "0"}],  ....}
   * => ["101","011"]
   */
-Algorithm.Fsm2schematic.index2truthTable = function(index, fsm) {
+Fsm2schematic.index2truthTable = function(index, fsm) {
   var arr = [];
   for (let state in fsm) {
     for (let input = 0; input <= 1; input++) {
@@ -171,10 +171,10 @@ Algorithm.Fsm2schematic.index2truthTable = function(index, fsm) {
   * fsm {"10": [{next: "00", out: "0"}, {next: "10", out: "0"}],  ....}
   * => [["101","011"],["001"]]
   */
-Algorithm.Fsm2schematic.fsm2stateTruthTable = function(fsm, bitLength) {
+Fsm2schematic.fsm2stateTruthTable = function(fsm, bitLength) {
   var arr = [];
   for (let i = 0; i < bitLength; i++)
-    arr.push(Algorithm.simplify(Algorithm.Fsm2schematic.index2truthTable(i, fsm)));
+    arr.push(Algorithm.simplify(Fsm2schematic.index2truthTable(i, fsm)));
   return arr;
 };
 
@@ -182,7 +182,7 @@ Algorithm.Fsm2schematic.fsm2stateTruthTable = function(fsm, bitLength) {
   * fsm {"10": [{next: "00", out: "0"}, {next: "10", out: "0"}],  ....}
   * => ["001","011"]
   */
-Algorithm.Fsm2schematic.fsm2outputTruthTable = function(fsm) {
+Fsm2schematic.fsm2outputTruthTable = function(fsm) {
   var arr = [];
   for (let state in fsm) {
     for (let input = 0; input <= 1; input++) {
@@ -193,15 +193,15 @@ Algorithm.Fsm2schematic.fsm2outputTruthTable = function(fsm) {
   return Algorithm.simplify(arr);
 };
 
-Algorithm.Fsm2schematic.removeUselessOutput = function(gates) {
+Fsm2schematic.removeUselessOutput = function(gates) {
   var gateNames = Object.keys(gates);
   for (let gate in gates)
-    Algorithm.Fsm2schematic.removeUselessPin(gateNames, gates[gate].out);
+    Fsm2schematic.removeUselessPin(gateNames, gates[gate].out);
   return gates;
 };
 
-// TODO: BUGS: not removing
-Algorithm.Fsm2schematic.removeUselessPin = function(gateNames, outpins) {
+// TODO: BUGS: not removing, should try removing multiple times
+Fsm2schematic.removeUselessPin = function(gateNames, outpins) {
   for (let i in outpins) {
     for (let j in outpins[i]) {
       if (typeof gateNames.indexOf(outpins[i][j].name) == -1)
@@ -210,14 +210,14 @@ Algorithm.Fsm2schematic.removeUselessPin = function(gateNames, outpins) {
   }
 };
 
-Algorithm.Fsm2schematic.removeNoOutputGates = function(gates) {
+Fsm2schematic.removeNoOutputGates = function(gates) {
   for (let gate in gates)
-    if (gate != "output" && Algorithm.Fsm2schematic.outputIsEmpty(gates[gate].out))
+    if (gate != "output" && Fsm2schematic.outputIsEmpty(gates[gate].out))
       delete gates[gate];
   return gates;
 }
 
-Algorithm.Fsm2schematic.outputIsEmpty = function(outputs) {
+Fsm2schematic.outputIsEmpty = function(outputs) {
   for (let output of outputs)
     if (typeof output !== 'undefined' && output.length > 0)
       return false;
--- js/algorithm/qm/QMAlgorithm.js
+++ js/algorithm/qm/QMAlgorithm.js
@@ -8,33 +8,34 @@
 
 var Algorithm = Algorithm || {};
 Algorithm.QMAlgorithm = Algorithm.QMAlgorithm || {};
+var QMAlgo = Algorithm.QMAlgorithm;
 
 // [0, 1, *, z] are reserved characters. Escape characters like [\, ', "] are also not used. 
-Algorithm.QMAlgorithm.ASCII_STRING ="ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmnopqrstuvwy{|}~!#$%&(),-./23456789:;<=>?@";
+QMAlgo.ASCII_STRING ="ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmnopqrstuvwy{|}~!#$%&(),-./23456789:;<=>?@";
 
 Algorithm.simplify = function(terms) {
   var bitLength = terms[0].length;
-  var output = Algorithm.QMAlgorithm.simplify(Algorithm.QMAlgorithm.generateInputs(bitLength), Algorithm.QMAlgorithm.parseInput(terms));
-  return Algorithm.QMAlgorithm.parseOutput(output, bitLength);
+  var output = QMAlgo.simplify(QMAlgo.generateInputs(bitLength), QMAlgo.parseInput(terms));
+  return QMAlgo.parseOutput(output, bitLength);
 };
 
 /**
   * "110" => "ABC*"
   */
-Algorithm.QMAlgorithm.term2expression = function(term) {
+QMAlgo.term2expression = function(term) {
   var str = "";
   for (let i = 0; i < term.length; i++)
-    str += Algorithm.QMAlgorithm.ASCII_STRING[i] + ((term[i] == 0) ? "*" : "");
+    str += QMAlgo.ASCII_STRING[i] + ((term[i] == 0) ? "*" : "");
   return str;
 };
 
 /**
   * ["001", "111"] => "A*B*C+ABC"
   */
-Algorithm.QMAlgorithm.parseInput = function(terms) {
+QMAlgo.parseInput = function(terms) {
   var arr = [];
   for (let i = 0; i < terms.length; i++)
-    arr.push(Algorithm.QMAlgorithm.term2expression(terms[i]));
+    arr.push(QMAlgo.term2expression(terms[i]));
 
   var expression = "";
   for (let i = 0; i < arr.length; i++) 
@@ -45,13 +46,13 @@ Algorithm.QMAlgorithm.parseInput = function(terms) {
 /**
   * ("A*B", 3) => "01x"
   */
-Algorithm.QMAlgorithm.expression2term = function(exp, bitLength) {
+QMAlgo.expression2term = function(exp, bitLength) {
   var ans = Array(bitLength).fill("x");
   for (let i = 0; i < exp.length; i++) {
     if (exp[i] == "*")
       continue;
     var isNegative = (i + 1 < exp.length && exp[i + 1] == "*");
-    ans[Algorithm.QMAlgorithm.ASCII_STRING.indexOf(exp[i])] = isNegative ? "0" : "1";
+    ans[QMAlgo.ASCII_STRING.indexOf(exp[i])] = isNegative ? "0" : "1";
   }
 
   var str = "";
@@ -63,20 +64,20 @@ Algorithm.QMAlgorithm.expression2term = function(exp, bitLength) {
 /**
   * "B + A*C" => ["x1x", "0x1"]
   */
-Algorithm.QMAlgorithm.parseOutput = function(expressionStr, bitLength) {
+QMAlgo.parseOutput = function(expressionStr, bitLength) {
   var expression = expressionStr.split(" + ");
   var arr = [];
   for (let i = 0; i < expression.length; i++)
-    arr.push(Algorithm.QMAlgorithm.expression2term(expression[i], bitLength));
+    arr.push(QMAlgo.expression2term(expression[i], bitLength));
   return arr;
 };
 
 /**
   * return ["A", "B", "C".....(# of bitLength)]
   */
-Algorithm.QMAlgorithm.generateInputs = function(bitLength) {
+QMAlgo.generateInputs = function(bitLength) {
   var inputs = [];
   for (let i = 0; i < bitLength; i++)
-    inputs.push(Algorithm.QMAlgorithm.ASCII_STRING[i])
+    inputs.push(QMAlgo.ASCII_STRING[i])
   return inputs;
 };
