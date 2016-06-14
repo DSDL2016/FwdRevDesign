b7ce5a0864a5d9eb7c63c2c9d0247856ce7eda2d
White <zkelly3@yahoo.com.tw>
2016-06-14 11:19:07 +0800

    fix bug

--- js/gate.js
+++ js/gate.js
@@ -1,64 +1,69 @@
 const Gate = {
     input: {
-        nIn: 0,
+        type: 'cmb',
+        nIn: 1,
         nOut: 1,
+        img: '',
         name: 'Input',
-        img: 'imgs/input.png'
+        truthTable: [
+            [0],
+            [1]
+        ]
     },
     or: {
+		type: 'cmb',        // 'cmb' => combinational, 'seq' => sequential
         nIn: 2,  // number of input pin
         nOut: 1, // number of output pin
         img: 'imgs/or.png', // background image
-        type: 'cmb',        // 'cmb' => combinational, 'seq' => sequential
         name: 'OR Gate',
-        truthTable: [ [[0, 1], [1, 1]] ] // or.truthTable[0][a][b] == a or b
+        truthTable: [[0], [1], [1], [1]] // or.truthTable[0][a][b] == a or b
     },
     and: {
+		type: 'cmb',        // 'cmb' => combinational, 'seq' => sequential
         nIn: 2,  // number of input pin
         nOut: 1, // number of output pin
         img: 'imgs/and.png', // background image
-        type: 'cmb',        // 'cmb' => combinational, 'seq' => sequential
         name: 'AND Gate',
-        truthTable: [ [[0, 0], [0, 1]] ] // and.truthTable[0][a][b] == a and b
+        truthTable: [[0], [0], [0], [1]] // and.truthTable[0][a][b] == a and b
     },
     not: {
+		type: 'cmb',        // 'cmb' => combinational, 'seq' => sequential
         nIn: 1,  // number of input pin
         nOut: 1, // number of output pin
         img: 'imgs/not.png', // background image
-        type: 'cmb',        // 'cmb' => combinational, 'seq' => sequential
         name: 'NOT Gate',
-        truthTable: [ [[1, 0]] ] // not.truthTable[0][a] == not a
+        truthTable: [[1], [0]] // not.truthTable[0][a] == not a
     },
     nand: {
+		type: 'cmb',        // 'cmb' => combinational, 'seq' => sequential
         nIn: 2,  // number of input pin
         nOut: 1, // number of output pin
         img: 'imgs/nand.png', // background image
-        type: 'cmb',        // 'cmb' => combinational, 'seq' => sequential
         name: 'NAND Gate',
-        truthTable: [ [[1, 1], [1, 0]] ] // nand.truthTable[0][a][b] == not(a and b)
+        truthTable: [[1], [1], [1], [0]] // nand.truthTable[0][a][b] == not(a and b)
     },
     nor: {
+		type: 'cmb',        // 'cmb' => combinational, 'seq' => sequential
         nIn: 2,  // number of input pin
         nOut: 1, // number of output pin
         img: 'imgs/nor.png', // background image
-        type: 'cmb',        // 'cmb' => combinational, 'seq' => sequential
         name: 'NOR Gate',
-        truthTable: [ [[1, 0], [0, 0]] ] // nor.truthTable[0][a][b] == not(a or b)
+        truthTable: [[1], [0], [0], [0]] // nor.truthTable[0][a][b] == not(a or b)
     },
     xor: {
+		type: 'cmb',        // 'cmb' => combinational, 'seq' => sequential
         nIn: 2,  // number of input pin
         nOut: 1, // number of output pin
         img: 'imgs/xor.png', // background image
-        type: 'cmb',        // 'cmb' => combinational, 'seq' => sequential
         name: 'XOR Gate',
-        truthTable: [ [[0, 1], [1, 0]] ] // xor.truthTable[0][a][b] == ((not a) and b) or (a and (not b))
+        truthTable: [[0], [1], [1], [0]] // xor.truthTable[0][a][b] == ((not a) and b) or (a and (not b))
     },
     rs: {
+		type: 'seq',
 		nIn: 2,
 		nOut: 2,
 		nState: 1,
 		img: 'imgs/RS.png',
-		type: 'seq',
 		name: 'RS Latch',
 		truthTable: {
 			first: [ // RSQ -> [Q^t+1]
@@ -78,12 +83,12 @@ const Gate = {
 		}
 	},
     jk: {
+	    type: 'seq',
         nIn: 2,
         nOut: 2,
 		nState: 1,
         name: 'JK Flip-Flop',
         img: './imgs/JK.png',
-        type: 'seq',
         truthTable: {
 			first: [ // JKQ-Q -> [Q^t+1]
 				[0], // 000
@@ -92,8 +97,8 @@ const Gate = {
 				[1], // 011
 				[0], // 100
 				[0], // 101
-				[1], //110
-				[0]  //111
+				[1], // 110
+				[0]  // 111
 			],
 			second: [ // Q -> [Q, -Q]
 				[0, 1],//0
@@ -102,12 +107,12 @@ const Gate = {
 		}
     },
     d: {
+	    type: 'seq',
         nIn: 1,
         nOut: 2,
 		nState: 1,
         name: 'D Flip-Flop',
         img: './imgs/D.png',
-        type: 'seq',
         truthTable: {
 			first: [ // DQ -> [Q^t+1]
 				[0], // 00
@@ -122,12 +127,12 @@ const Gate = {
 		}
     },
     t: {
+	    type: 'seq',
         nIn: 1,
         nOut: 2,
 		nState: 1,
         name: 'T Flip-Flop',
         img: './imgs/T.png',
-        type: 'seq',
         truthTable: {
 			first: [ // TQ -> [Q^t+1]
 				[0], // 00
@@ -140,5 +145,12 @@ const Gate = {
 				[1, 0] //1
 			]
 		}
+    },
+	output: {
+        type: 'cmb',
+        nIn: 1,
+        nOut: 0,
+		img: '',
+        truthTable: [[],[]]
     }
 };
