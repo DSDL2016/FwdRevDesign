fb82b930d4a895289a4e1c39b3c641d83791625a
ctinray <b03902072@ntu.edu.tw>
2017-03-23 22:02:31 +0800

    add github links :D

--- js/gui/init.js
+++ js/gui/init.js
@@ -1,5 +1,8 @@
 
 $(document).ready(function(){
+    if (window.confirm('If you like our work, please give us a star :D')) {
+        window.location = 'https://github.com/DSDL2016/FwdRevDesign';
+    }
     GUI.schematic.init($('#schematicPaper'));
     GUI.fsm.init($('#fsmPaper'));      
     GUI.view.init();
